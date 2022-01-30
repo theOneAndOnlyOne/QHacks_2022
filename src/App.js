da824a5980af8079843d79e9e276a9c1afb45687
Joshua <60112951+theOneAndOnlyOne@users.noreply.github.com>
2022-01-29 23:56:52 -0500

    added app.js and server.js interaction

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import Spotify from 'react-spotify-embed';
 import { ReactComponent as Hamburger } from './icons/hamburgerMenu.svg';
 import { ReactComponent as About} from './icons/i.svg';
 import Form from './Form';
+import axios from 'axios';
+import { userTable } from 'react-table';
 //import { ReactComponent as Diamond } from './icons/diamond.svg';
+//import Server from './server/server'
 
 import {CSSTransition} from 'react-transition-group';
 
 
-const spotifySongLink = "https://open.spotify.com/track/1EWLulTbAPFXrpj9gLcWFT?si=53ab72bf46c64bc0";
+const spotifySongLink = "https://open.spotify.com/track/6WC3MB4KyAJ1KhGuqzq0W2?si=b1861bfdccc740da";
+//let singerId = "1Xyo4u8uXC1ZmMpatF05PJ";
+let singerInfo = [
+  {
+    "name": "A$AP Rocky",
+    "id": "13ubrt8QOOCPljQ2FL1Kca",
+    "spotify": "https://open.spotify.com/artist/13ubrt8QOOCPljQ2FL1Kca"
+
+  },
+  {
+    "name": "Childish Gambino",
+    "id": "73sIBHcqh3Z3NyqHKZ7FOL",
+    "spotify": "https://open.spotify.com/artist/73sIBHcqh3Z3NyqHKZ7FOL"
+  }
+];
 
 function App() {
   return (
@@ -26,21 +43,46 @@ function App() {
   );
 }
 
+
+
 function SpotifyPlugin() {
+  
+
+  const sayHello = async () => {
+    try {
+      const res = await axios.get('http://localhost:3001/related?id=1Xyo4u8uXC1ZmMpatF05PJ');
+      const artistsId = res.data.artists;
+      let data = artistsId;
+      console.log(singerInfo);
+      singerInfo =[]
+      for (let i = 0; i < data.length; i++)
+      {
+        singerInfo.push({'name':data[i].name, 'id':data[i].id, 'spotify':data[i].external_urls.spotify});
+      }
+      console.log(singerInfo);
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className = "background">
       <div className = "embed">
         <Spotify link= {spotifySongLink}/>
+        
       </div>
-        <div>
-          <button className = "surpriseButton">Surprise Me</button>
-        </div>
+      <form onSubmit ={sayHello}>
+          <label name="input-text" id="input-text" required spellcheck="false"> Enter Spotify ID
+            <input type = "text"/>
+          </label>
+      </form>
+      <div>
+        <button className = "surpriseButton" onClick={sayHello}>Enter Spotify ID</button>
+      </div>
       
     </div>
     
   );
 }
-
 
 function Navbar(props) {
   return (
@@ -91,6 +133,17 @@ function DropdownMenu() {
     )
   }
 
+  function LoginButton(props) {
+    return (
+      <a href ="#" className='menu-item'>
+        <span className='icon-button'>{props.leftIcon}</span>
+        {props.children}
+
+        <span className = "icon-right">{props.rightIcon}</span>
+      </a>
+    )
+  }
+
   return (
     <div className = "dropdown" style = {{ height: menuHeight}}>
       <CSSTransition 
@@ -101,9 +154,7 @@ function DropdownMenu() {
         onEnter={calcHeight}
         >
           <div className = "menu">
-
-            <DropdownItem> My Profile </DropdownItem>
-            
+            <LoginButton> My Profile </LoginButton>
             <DropdownItem
               className='icon-button' 
               leftIcon = {<About/>}
